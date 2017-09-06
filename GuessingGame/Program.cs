d39023a78612ee0fe5319c4c95e7666beae595e3
Firefly4266 <gaines.johnp@gmail.com>
2017-09-05 17:58:22 -0700

    completed guessing game exercise

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,6 +10,42 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            string playerInput;
+            int playerGuess;
+            bool isNumberGuessed = false;
+            int answer;
+
+            Console.Write("Guess a number between 1 and 20: ");
+            playerInput = Console.ReadLine();
+
+
+            Random randomNum = new Random();
+            answer = randomNum.Next(1, 21);
+            while (!isNumberGuessed)
+            {
+                //int.TryParse(playerInput, out playerGuess);
+                playerGuess = int.Parse(playerInput);
+                if (answer == playerGuess)
+                {
+                    isNumberGuessed = true;
+                    Console.WriteLine("Your are correct, the answer is {0}", answer);
+                    break;
+                }
+                else
+                    if (playerGuess < answer)
+                    {
+                        string low = "You guessed too low.";
+                        Console.WriteLine("{0} is incorrect. {1} Try again.", playerGuess, low);
+                    }
+                    else
+                    {
+                        string high = "you've guessed too high.";
+                        Console.WriteLine("{0} is incorrect. {1} Try again.", playerGuess, high);
+                    }
+                Console.Write("Guess a number between 1 and 20: ");
+                playerInput = Console.ReadLine();
+            }
+            Console.WriteLine("You guessed correct, {0} is the correct number!", answer);
         }
     }
 }
